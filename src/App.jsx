8a4cf182995c292e28a93e5b913414216f2a47bf
yashdatir1999 <yashdatir1999@gmail.com>
2024-01-14 18:56:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './componets/Header'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Header />
+    </div>
+  )
+}
+
+export default App
