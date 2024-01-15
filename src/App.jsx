c3061c79ea721794e806f2a8332a08fb763099a6
yashdatir1999 <yashdatir1999@gmail.com>
2024-01-15 10:04:38 +0530

    slider complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './componets/Header'
+import Slider from './componets/Slider'
 
 const App = () => {
   return (
     <div className=''>
       <Header />
+      <Slider />
     </div>
   )
 }
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const movieBaseUrl = "https://api.themoviedb.org/3"
+// const movieBaseUrl = "https://api.themoviedb.org/3"
+// console.log(movieBaseUrl)
 
-const api_key='e83762597b8833b66a7569199e1bea84'
+// const api_key='e83762597b8833b66a7569199e1bea84'
 
-// https://api.themoviedb.org/3/trending/all/day?api_key=e83762597b8833b66a7569199e1bea84
+//api.themoviedb.org/3/trending/all/day?api_key=e83762597b8833b66a7569199e1bea84
 
-const getTrendingVideos=axios.get(this.movieBaseUrl+"/trending/all/day?api_key"+api_key)
+// const getTrendingVideos=axios.get(this.movieBaseUrl+"/trending/all/day?api_key"+api_key)
+
+const getTrendingVideos=axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=e83762597b8833b66a7569199e1bea84")
 
 export default{getTrendingVideos}
+
